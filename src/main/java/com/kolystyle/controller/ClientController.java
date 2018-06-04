bcd528d51ecd868f68eea330a7a09d7dedd1478a
khanaldeepak1988 <khanaldeepak1988@gmail.com>
2018-06-04 04:10:04 -0400

    Added Pagination for products

--- src/main/java/com/kolystyle/controller/ClientController.java
+++ src/main/java/com/kolystyle/controller/ClientController.java
@@ -6,23 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.kolystyle.domain.PagerModel;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import com.kolystyle.repository.ProductsRepository;
+import com.kolystyle.service.SiteSettingService;
 import com.kolystyle.domain.Product;
+import com.kolystyle.domain.SiteSetting;
 @Controller
 public class ClientController {
     private static final int BUTTONS_TO_SHOW = 3;
     private static final int INITIAL_PAGE = 0;
-    private static final int INITIAL_PAGE_SIZE = 5;
-    private static final int[] PAGE_SIZES = { 5, 10};
+    private static final int INITIAL_PAGE_SIZE = 20;
+    private static final int[] PAGE_SIZES = { 20, 50, 100};
     @Autowired
     ProductsRepository productsRepository;
+    @Autowired
+    SiteSettingService siteSettingService;
     @GetMapping("/products")
     public ModelAndView homepage(@RequestParam("pageSize") Optional<Integer> pageSize,
-            @RequestParam("page") Optional<Integer> page){
-        
+            @RequestParam("page") Optional<Integer> page,Model model){
+    	SiteSetting siteSettings = siteSettingService.findOne(new Long(1));
+        model.addAttribute("siteSettings",siteSettings);
         ModelAndView modelAndView = new ModelAndView("products");
         //
         // Evaluate page size. If requested parameter is null, return initial
