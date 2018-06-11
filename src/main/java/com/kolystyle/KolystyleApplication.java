6e7634be123858cfb425488913094245fcacd877
khanaldeepak1988 <khanaldeepak1988@gmail.com>
2018-06-10 22:46:16 -0400

    Connected to AWS DB 13.0.1

--- src/main/java/com/kolystyle/KolystyleApplication.java
+++ src/main/java/com/kolystyle/KolystyleApplication.java
@@ -1,3 +1,23 @@
+/*package com.kolystyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class KolystyleApplication extends SpringBootServletInitializer {
+	 
+	 public static void main(String[] args) throws Exception {
+	        SpringApplication.run(KolystyleApplication.class, args);
+	    }
+	 
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(KolystyleApplication.class);
+	    }
+
+}*/
 package com.kolystyle;
 
 import org.springframework.boot.CommandLineRunner;
