2215d10a98f91c6dfa02231403aecf9db3cb7787
khanaldeepak1988 <khanaldeepak1988@gmail.com>
2018-06-18 01:45:07 -0400

    working on ses email

---
+++ src/main/java/com/kolystyle/config/ThymeleafConfigurator.java
@@ -0,0 +1,28 @@
+package com.kolystyle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafConfigurator {
+
+	/**
+	 * <p>emailTemplateResolver.</p>
+	 *
+	 * @return a {@link org.thymeleaf.templateresolver.ClassLoaderTemplateResolver} object.
+	 */
+	@Bean
+	public ClassLoaderTemplateResolver emailTemplateResolver() {
+		ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+		emailTemplateResolver.setPrefix("mails/");
+		emailTemplateResolver.setSuffix(".html");
+		emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+		emailTemplateResolver.setCharacterEncoding("UTF-8");
+		emailTemplateResolver.setOrder(0);
+		emailTemplateResolver.setCheckExistence(true);
+
+		return emailTemplateResolver;
+	}
+}
